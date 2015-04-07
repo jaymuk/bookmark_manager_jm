106cc5eae7e11be5b2e041b3690a601a4d642027
jaymuk <jaymuk.business@gmail.com>
2015-04-07 20:09:51 +0100

    Added a test for adding tags to links

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -19,4 +19,23 @@ feature "User adds a new link" do
     end
   end
 
+  scenario "with a few tags" do
+  visit "/"
+  add_link("http://www.makersacademy.com/", "Makers", ['education', 'ruby'])
+  link = Link.first
+  expect(link.tags).to include("education")
+  expect(link.tags).to include("ruby")
+  end
+
+  def add_link(url, title, tags = [])
+    within('#new-link') do
+      fill_in 'url', :with => url
+      fill_in 'title', :with => title
+      # our tags will be space separated
+      fill_in 'tags', :with => tags.join(' ')
+      click_button 'Add link'
+    end 
+  end
 end
+
+
